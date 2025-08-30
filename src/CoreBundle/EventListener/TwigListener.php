webstone/chamilo-lms

commit dc1ae7126889bf1de5537c7ba990ca2ac630e0fa

Add access URL chooser for users with multiple active portals - refs BT#22639
* Replacing usage of UrlManager::get_url_data with EntityRepository::findAll - refs BT#22639

* Replace hardcoded localhost URL with AccessUrl::DEFAULT_ACCESS_URL - refs BT#22639

* Enhance access URL management to integrate the login-only option - refs BT#22639

* Add access URL chooser for users with multiple active portals - refs BT#22639

* Set Access-Control-Allow-Credentials header for login_token_check route only - refs BT#22639

* Implement login token authentication to use access URL chooser - refs BT#22639

* Add AccessUrlListener to manage unique login-only url and save the access url hierarchy - refs BT#22639

* Set login-only URL as parent for other access URLs, except the main one - refs BT#22639

Implemented logic to assign the login-only access URL as the parent for other access URLs, excluding the main URL

* fix edit access url - refs BT#22639

* Refactor delete method to use EntityManager for cascading deletions of related access URLs - refs BT#22639

* Vendor: Require jeremykendall/php-domain-parser - refs BT#22639

* Refactor URL existence check to use repository method and add domain validation for login-only URLs - refs BT#22639

* Refactor messages to show when choosing an access URL - refs BT#22639

* Minor: Add translations - refs BT#22639

src/CoreBundle/EventListener/TwigListener.php
@@ -6,10 +6,11 @@ declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\EventListener;
 
+use Chamilo\CoreBundle\Helpers\AccessUrlHelper;
+use Chamilo\CoreBundle\Helpers\UserHelper;
 use Chamilo\CoreBundle\Repository\LanguageRepository;
+use Chamilo\CoreBundle\Repository\Node\AccessUrlRepository;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Twig\Environment;
 
@@ -21,25 +22,23 @@ class TwigListener
     public function __construct(
         private readonly Environment $twig,
         private readonly SerializerInterface $serializer,
-        private readonly TokenStorageInterface $tokenStorage,
         private readonly LanguageRepository $languageRepository,
+        private readonly UserHelper $userHelper,
+        private readonly AccessUrlHelper $accessUrlHelper,
     ) {}
 
     public function __invoke(ControllerEvent $event): void
     {
-        $request = $event->getRequest();
-        $token = $this->tokenStorage->getToken();
+        $currentAccessUrl = $this->accessUrlHelper->getCurrent();
+        $user = $this->userHelper->getCurrent();
 
         $data = null;
         $isAuth = false;
-        if (null !== $token) {
-            $user = $token->getUser();
-            if ($user instanceof UserInterface) {
-                $data = $this->serializer->serialize($user, 'jsonld', [
-                    'groups' => ['user_json:read'],
-                ]);
-                $isAuth = true;
-            }
+        if ($user) {
+            $data = $this->serializer->serialize($user, 'jsonld', [
+                'groups' => ['user_json:read'],
+            ]);
+            $isAuth = true;
         }
 
         $languages = $this->languageRepository->getAllAvailable()->getQuery()->getArrayResult();
@@ -47,7 +46,8 @@ class TwigListener
         // $this->twig->addGlobal('text_direction', api_get_text_direction());
         $this->twig->addGlobal('is_authenticated', json_encode($isAuth));
         $this->twig->addGlobal('user_json', $data ?? json_encode([]));
-        $this->twig->addGlobal('access_url_id', $request->getSession()->get('access_url_id'));
+        $this->twig->addGlobal('is_login_url', (int) $currentAccessUrl->isLoginOnly());
+        $this->twig->addGlobal('access_url_id', $currentAccessUrl->getId());
         $this->twig->addGlobal('languages_json', json_encode($languages));
     }
 }

commit c0a848b39be7a8dec4334597e0a5216fed8cd7db

Install: Fix support for default error page on undefined URL in multi-URL, broken in dc1ae712 - refs #6297

src/CoreBundle/EventListener/TwigListener.php
@@ -45,8 +45,14 @@ class TwigListener
         // $this->twig->addGlobal('text_direction', api_get_text_direction());
         $this->twig->addGlobal('is_authenticated', json_encode($isAuth));
         $this->twig->addGlobal('user_json', $data ?? json_encode([]));
-        $this->twig->addGlobal('is_login_url', (int) $currentAccessUrl->isLoginOnly());
-        $this->twig->addGlobal('access_url_id', $currentAccessUrl->getId());
+        if (!empty($currentAccessUrl)) {
+            $this->twig->addGlobal('is_login_url', (int) $currentAccessUrl->isLoginOnly());
+            $this->twig->addGlobal('access_url_id', $currentAccessUrl->getId());
+        } else {
+            // when the URL has not been defined in the access_url table, we need some fallback mechanism
+            $this->twig->addGlobal('is_login_url', 0);
+            $this->twig->addGlobal('access_url_id', 1);
+        }
         $this->twig->addGlobal('languages_json', json_encode($languages));
     }
 }