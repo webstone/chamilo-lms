3324377e3e9ba58348080eb4fa723b5c5e3fa7ea
Gregory V <github@addow.be>
2026-05-23 01:00:53 +0200

    feat(course): add SetCourseIllustrationAction for multipart illustration upload

---
+++ src/CoreBundle/Controller/Api/SetCourseIllustrationAction.php
@@ -0,0 +1,39 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
+
+namespace Chamilo\CoreBundle\Controller\Api;
+
+use Chamilo\CoreBundle\Entity\Course;
+use Chamilo\CoreBundle\Entity\User;
+use Chamilo\CoreBundle\Repository\Node\IllustrationRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+final class SetCourseIllustrationAction
+{
+    public function __construct(
+        private readonly IllustrationRepository $illustrationRepository,
+        private readonly Security $security,
+    ) {}
+
+    public function __invoke(Request $request, Course $data): Course
+    {
+        $uploadedFile = $request->files->get('uploadFile');
+        if ($uploadedFile === null) {
+            throw new BadRequestHttpException('"uploadFile" is required');
+        }
+
+        $creator = $this->security->getUser();
+        if (!$creator instanceof User) {
+            throw new BadRequestHttpException('Authenticated user required');
+        }
+
+        $this->illustrationRepository->addIllustration($data, $creator, $uploadedFile);
+
+        return $data;
+    }
+}
