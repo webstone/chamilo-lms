69a987bcada275d98c0f854c4ff3ac57bbc1f034
Gregory V <github@addow.be>
2026-05-23 01:04:06 +0200

    feat(course): wire Set/RemoveCourseIllustrationAction to /api/courses/{id}/illustration

--- src/CoreBundle/Entity/Course.php
+++ src/CoreBundle/Entity/Course.php
@@ -11,14 +11,20 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Operation as OpenApiOperation;
+use ApiPlatform\OpenApi\Model\RequestBody;
+use ArrayObject;
 use Chamilo\CoreBundle\State\CourseStateProcessor;
 use ApiPlatform\OpenApi\Model\Parameter;
 use Chamilo\CoreBundle\Controller\Api\GetCourseStatsAction;
+use Chamilo\CoreBundle\Controller\Api\RemoveCourseIllustrationAction;
+use Chamilo\CoreBundle\Controller\Api\SetCourseIllustrationAction;
 use Chamilo\CoreBundle\Entity\Listener\CourseListener;
 use Chamilo\CoreBundle\Entity\Listener\ResourceListener;
 use Chamilo\CoreBundle\Filter\ExtraFieldFilter;
@@ -85,6 +91,34 @@ use const SORT_NATURAL;
         new Patch(
             security: "is_granted('ROLE_ADMIN')",
             processor: CourseStateProcessor::class,
+        ),
+        new Post(
+            uriTemplate: '/courses/{id}/illustration',
+            controller: SetCourseIllustrationAction::class,
+            openapi: new OpenApiOperation(
+                summary: 'Set the illustration image for a Course',
+                requestBody: new RequestBody(
+                    content: new ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'uploadFile' => ['type' => 'string', 'format' => 'binary'],
+                                ],
+                            ],
+                        ],
+                    ]),
+                ),
+            ),
+            security: "is_granted('ROLE_ADMIN')",
+            deserialize: false,
+        ),
+        new Delete(
+            uriTemplate: '/courses/{id}/illustration',
+            controller: RemoveCourseIllustrationAction::class,
+            security: "is_granted('ROLE_ADMIN')",
+            deserialize: false,
+            output: false,
         ),
         new GetCollection(
             paginationClientEnabled: true,
